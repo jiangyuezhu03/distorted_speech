9dba65d3855e23edc465d11f5cc77c5fe1c3b8f9
Jiangyue Zhu <jiangyue.zhu@outlook.com>
2025-07-20 20:40:53 +0100

    added wavlm results

--- code/ds_test.py
+++ code/ds_test.py
@@ -4,4 +4,5 @@ from datasets import load_from_disk,load_dataset
 # new_adjusted_ds = load_from_disk("/work/tc068/tc068/jiangyue_zhu/ted3test_distorted_adjusted/narrowband_adjusted/narrowband_high_mid_1_3")
 # print(original_distorted_ds)
 # print(new_adjusted_ds)
-ted3train=load_from_disk("")
+ted3test=load_from_disk("ted3test_distorted/")
+print(ted3test)
